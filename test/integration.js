13dd17a4964c6def8e54e6eb4c59713869291889
Excodibur <excodibur@posteo.de>
2021-08-08 17:15:49 +0200

    removed test settings from default configuration

--- test/integration.js
+++ test/integration.js
@@ -32,26 +32,36 @@ tests.integration(path.join(__dirname, ".."), {
                 return new Promise(async (resolve, reject) => {
                     const harness = getHarness();
 
-                    await harness.startAdapterAndWait()
+                    harness._objects.getObject("system.adapter.schwoerer-ventcube.0", async (err, obj) => {
+                        if (err)
+                            reject(new Error("Can't modify adapter configuration to prepare testcase. Error: " + err));
 
-                    //check if states shown correlate to mock server
-                    //read data from mock logs
-                    let mockValues = readValuesFromMock();
+                        obj.native.server = "localhost";
+                        obj.native.port = 10502;
 
-                    //get state from adapter, adapter need some time to load first values from mock
-                    await delay (15000);
-                    
-                    //Check one value from mockservice and compare to state in adapter. In theory we could iterate over all values
-                    //but let's keep it simple for now.
-                    harness.states.getState("schwoerer-ventcube.0.parameters.base-temp-room-1", function (error, state){
-                        //Special treatment is needed for this value specifically, as measure-unit is °C and adapter divides it
-                        //by 10 (by design).
-                        let compareAdapterValue = state.val * 10;
-                        if (compareAdapterValue == mockValues['base-temp-room-1'])
-                            resolve();
-                        else {
-                            reject("ERROR - Value Missmatch. Function: base-temp-room-1, Adapter value: " + compareAdapterValue + ", Mock value:" + mockValues['base-temp-room-1']);
-                        }
+                        harness._objects.setObject(obj._id, obj);
+
+                        await harness.startAdapterAndWait()
+
+                        //check if states shown correlate to mock server
+                        //read data from mock logs
+                        let mockValues = readValuesFromMock();
+
+                        //get state from adapter, adapter need some time to load first values from mock
+                        await delay (15000);
+                        
+                        //Check one value from mockservice and compare to state in adapter. In theory we could iterate over all values
+                        //but let's keep it simple for now.
+                        harness.states.getState("schwoerer-ventcube.0.parameters.base-temp-room-1", function (error, state){
+                            //Special treatment is needed for this value specifically, as measure-unit is °C and adapter divides it
+                            //by 10 (by design).
+                            let compareAdapterValue = state.val * 10;
+                            if (compareAdapterValue == mockValues['base-temp-room-1'])
+                                resolve();
+                            else {
+                                reject("ERROR - Value Missmatch. Function: base-temp-room-1, Adapter value: " + compareAdapterValue + ", Mock value:" + mockValues['base-temp-room-1']);
+                            }
+                        });
                     });
 
                 });
@@ -61,35 +71,45 @@ tests.integration(path.join(__dirname, ".."), {
                 return new Promise(async (resolve, reject) => {
                     const harness = getHarness();
 
-                    await harness.startAdapterAndWait()
+                    harness._objects.getObject("system.adapter.schwoerer-ventcube.0", async (err, obj) => {
+                        if (err)
+                            reject(new Error("Can't modify adapter configuration to prepare testcase. Error: " + err));
 
-                    //TODO: perhaps read old value from mock first to ensure it is really changed
-                    let mockValues = readValuesFromMock();
-                    let initialBaseTempRoom1 = mockValues['base-temp-room-1'] / 10;
-                    let newBaseTempRoom1Rnd = initialBaseTempRoom1;
+                        obj.native.server = "localhost";
+                        obj.native.port = 10502;
 
-                    while (newBaseTempRoom1Rnd == initialBaseTempRoom1)
-                        newBaseTempRoom1Rnd = Math.floor(Math.random() * 30) + 10; //minValue = 10, maxvalue = 30
-                    
-                    //get state from adapter, adapter need some time to load first values from mock
-                    await delay (15000);
+                        harness._objects.setObject(obj._id, obj);
 
-                    await harness.states.setStateAsync("schwoerer-ventcube.0.parameters.base-temp-room-1", newBaseTempRoom1Rnd);
-                    //check if states shown correlate to mock server
-                    
+                        await harness.startAdapterAndWait()
 
-                    // give adapter time to send new value to
-                    await delay (2000);
+                        //TODO: perhaps read old value from mock first to ensure it is really changed
+                        let mockValues = readValuesFromMock();
+                        let initialBaseTempRoom1 = mockValues['base-temp-room-1'] / 10;
+                        let newBaseTempRoom1Rnd = initialBaseTempRoom1;
 
-                    //read data from mock logs
-                    mockValues = readValuesFromMock();
+                        while (newBaseTempRoom1Rnd == initialBaseTempRoom1)
+                            newBaseTempRoom1Rnd = Math.floor(Math.random() * 30) + 10; //minValue = 10, maxvalue = 30
+                        
+                        //get state from adapter, adapter need some time to load first values from mock
+                        await delay (15000);
 
-                    if (mockValues['base-temp-room-1'] == (newBaseTempRoom1Rnd*10)) {
-                        console.log("SUCCESS - Old base-temp-room-1 was: " + initialBaseTempRoom1 + " now: " + newBaseTempRoom1Rnd);
-                        resolve();
-                    } else {
-                        reject("ERROR - Value for function base-temp-room-1 should have been updated to 33, but in Mock actually is " + mockValues['base-temp-room-1'])
-                    }
+                        await harness.states.setStateAsync("schwoerer-ventcube.0.parameters.base-temp-room-1", newBaseTempRoom1Rnd);
+                        //check if states shown correlate to mock server
+                        
+
+                        // give adapter time to send new value to
+                        await delay (2000);
+
+                        //read data from mock logs
+                        mockValues = readValuesFromMock();
+
+                        if (mockValues['base-temp-room-1'] == (newBaseTempRoom1Rnd*10)) {
+                            console.log("SUCCESS - Old base-temp-room-1 was: " + initialBaseTempRoom1 + " now: " + newBaseTempRoom1Rnd);
+                            resolve();
+                        } else {
+                            reject("ERROR - Value for function base-temp-room-1 should have been updated to 33, but in Mock actually is " + mockValues['base-temp-room-1'])
+                        }
+                    });
                 });
 
             }).timeout(30000);;
